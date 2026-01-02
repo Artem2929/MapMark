8c19befef1e74229539f99c54a2be484fa1d83b9
Artem Polishchuk <artempolishchuk@MacBook-Air-Artem.local>
2026-01-02 15:56:23 +0200

    refactor: оновлено іконку та стилі кнопки редагування аватара
    - Замінено іконку камери на сучасну іконку фото
    - Застосовано стилі як у photo-like-btn
    - Прозорий фон з backdrop-filter blur
    - Білий колір іконки
    - Плавна анімація scale при hover

--- src/features/profile/components/ProfileHeader.jsx
+++ src/features/profile/components/ProfileHeader.jsx
@@ -131,7 +131,8 @@ const ProfileHeader = memo(({ user, isOwnProfile, onUserUpdate, onEditingStateCh
                 onClick={() => setShowAvatarModal(true)}
               >
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3z"/>
+                  <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
+                  <circle cx="16.5" cy="8.5" r="1.5"/>
                 </svg>
               </button>
             )}
